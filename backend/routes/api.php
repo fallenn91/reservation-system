befd159a10b1fb6313475cf4f319a5f38a35c07f
Nuria Feng Almendros González <a243371na@correu.escoladeltreball.org>
2026-04-05 15:50:51 +0200

    fixing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,4 +4,8 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
+Route::options('/contacto', function () {
+    return response()->json([], 200);
+});
+
 Route::post('/contacto', [ContactController::class, 'sendForm']);
